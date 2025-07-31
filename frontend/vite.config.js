600b97af6dffa8668b66f0aa8806339144a6549b
Omadli Yigit <76625693+omadli@users.noreply.github.com>
2025-07-31 13:26:36 +0500

    remove unused libraries, optimize build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'terser',
+    sourcemap: false,
+    brotliSize: false,
+    cssCodeSplit: true,
+  },
   plugins: [react()],
 });
